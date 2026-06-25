soerengrunewald/powercap

commit 9e3f11e97058050c040089316039e342c7e7d11c

Use filesystem::path for file names
[Why]
It makes it more obvious what the argument is ;)

Signed-off-by: Soeren Grunewald <soeren.grunewald@gmx.net>

main.cc
@@ -39,7 +39,7 @@ namespace {
 		return hsl >= hl and std::string_view{ str.data(), hl }.compare(prefix) == 0;
 	}
 
-	std::optional<std::string> read_string_from(std::string const& p) {
+	std::optional<std::string> read_string_from(fs::path const& p) {
 		std::ifstream f(p);
 		if (not f.is_open())
 			return {};
@@ -48,7 +48,7 @@ namespace {
 		return s;
 	}
 
-	std::optional<std::uint64_t> read_dec_uint64_value_from(std::string const& p) {
+	std::optional<std::uint64_t> read_dec_uint64_value_from(fs::path const& p) {
 		auto v = read_string_from(p);
 		if (v.has_value()) try {
 			return std::stoul(v.value());
@@ -58,7 +58,7 @@ namespace {
 		return {};
 	}
 
-	inline int write_dec_uint64_value_to(std::string const& p, std::uint64_t v) {
+	inline int write_dec_uint64_value_to(fs::path const& p, std::uint64_t v) {
 		std::ofstream f{p};
 		if (not f.is_open())
 			return -EPERM;
@@ -67,7 +67,7 @@ namespace {
 		return 0;
 	}
 
-	inline int write_dec_uint64_value_to(std::string const& p, std::optional<std::uint64_t> const& v) {
+	inline int write_dec_uint64_value_to(fs::path const& p, std::optional<std::uint64_t> const& v) {
 		if (not v.has_value())
 			return -ENODATA;
 		return write_dec_uint64_value_to(p, v.value());
@@ -88,8 +88,8 @@ namespace {
 	}
 
 	// Try to figure the hwmon entry
-	std::string find_hwmon_base_path(std::string const& p) {
-		fs::path const base_path{ p + "/device/hwmon" };
+	std::string find_hwmon_base_path(fs::path const& p) {
+		fs::path const base_path{ p / "device/hwmon" };
 		for (auto const& dir_entry : fs::directory_iterator{ base_path }) {
 			if (not dir_entry.is_directory())
 				continue;

commit f50c1d75ee0d742be88fb6cc42cfc670de05ebae

Avoid unnecessary string <-> path conversions
[Why]
A while back we started using std::filesystem::path over std::string,
but our conversion was not complete.

Fixes: 9e3f11e970 (Use filesystem::path for file names)
Signed-off-by: Soeren Grunewald <soeren.grunewald@gmx.net>

main.cc
@@ -76,7 +76,7 @@ namespace {
 	}
 
 	// Try to find the first card entry
-	std::string find_card_base_path() {
+	fs::path find_card_base_path() {
 		fs::path const base_path{ "/sys/class/drm" };
 		for (auto const& dir_entry : fs::directory_iterator{ base_path }) {
 			if (not dir_entry.is_directory())
@@ -85,22 +85,22 @@ namespace {
 			auto const f = p.filename().string();
 			if (not starts_with(f, "card") or contains(f, "-"))
 				continue;
-			return p.string();
+			return p;
 		}
-		return "";
+		return {};
 	}
 
 	// Try to figure the hwmon entry
-	std::string find_hwmon_base_path(fs::path const& p) {
+	fs::path find_hwmon_base_path(fs::path const& p) {
 		fs::path const base_path{ p / "device/hwmon" };
 		if (not fs::exists(base_path))
-			return "";
+			return {};
 		for (auto const& dir_entry : fs::directory_iterator{ base_path }) {
 			if (not dir_entry.is_directory())
 				continue;
-			return dir_entry.path().string();
+			return dir_entry.path();
 		}
-		return "";
+		return {};
 	}
 
 	enum Action {
@@ -164,8 +164,8 @@ int main(int argc, char* argv[])
 		"/power1_cap_max"
 	};
 
-	auto pwrtarget = read_dec_uint64_value_from(hwmon + std::string{ pwr_source[what_to_do] });
-	auto err = write_dec_uint64_value_to(hwmon + "/power1_cap", pwrtarget);
+	auto pwrtarget = read_dec_uint64_value_from(hwmon / pwr_source[what_to_do]);
+	auto err = write_dec_uint64_value_to(hwmon / "/power1_cap", pwrtarget);
 	if (err < 0)
 		std::cerr << "Could not write: " << std::strerror(-err) << std::endl;
 