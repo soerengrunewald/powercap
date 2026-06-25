soerengrunewald/powercap

commit f50c1d75ee0d742be88fb6cc42cfc670de05ebae

Avoid unnecessary string <-> path conversions
[Why]
A while back we started using std::filesystem::path over std::string,
but our conversion was not complete.

Fixes: 9e3f11e970 (Use filesystem::path for file names)
Signed-off-by: Soeren Grunewald <soeren.grunewald@gmx.net>

main.cc
@@ -76,7 +76,7 @@ namespace {
 	}
 
 	// Try to find the first card entry
-	std::string find_card_base_path() {
+	fs::path find_card_base_path() {
 		fs::path const base_path{ "/sys/class/drm" };
 		for (auto const& dir_entry : fs::directory_iterator{ base_path }) {
 			if (not dir_entry.is_directory())
@@ -85,22 +85,22 @@ namespace {
 			auto const f = p.filename().string();
 			if (not starts_with(f, "card") or contains(f, "-"))
 				continue;
-			return p.string();
+			return p;
 		}
-		return "";
+		return {};
 	}
 
 	// Try to figure the hwmon entry
-	std::string find_hwmon_base_path(fs::path const& p) {
+	fs::path find_hwmon_base_path(fs::path const& p) {
 		fs::path const base_path{ p / "device/hwmon" };
 		if (not fs::exists(base_path))
-			return "";
+			return {};
 		for (auto const& dir_entry : fs::directory_iterator{ base_path }) {
 			if (not dir_entry.is_directory())
 				continue;
-			return dir_entry.path().string();
+			return dir_entry.path();
 		}
-		return "";
+		return {};
 	}
 
 	enum Action {
@@ -164,8 +164,8 @@ int main(int argc, char* argv[])
 		"/power1_cap_max"
 	};
 
-	auto pwrtarget = read_dec_uint64_value_from(hwmon + std::string{ pwr_source[what_to_do] });
-	auto err = write_dec_uint64_value_to(hwmon + "/power1_cap", pwrtarget);
+	auto pwrtarget = read_dec_uint64_value_from(hwmon / pwr_source[what_to_do]);
+	auto err = write_dec_uint64_value_to(hwmon / "/power1_cap", pwrtarget);
 	if (err < 0)
 		std::cerr << "Could not write: " << std::strerror(-err) << std::endl;
 

commit 79d48574a86b658e67030b424dc3838d685fed15

Fix path composing after avoiding strings
[Why]
When we switch from strings to use path objects, we forgot that we
now compose path objects and therefor the path will take care of
the path separator.

Fixes: f50c1d75e (Avoid unnecessary string <-> path conversions)
Signed-off-by: Soeren Grunewald <soeren.grunewald@gmx.net>

main.cc
@@ -161,13 +161,13 @@ int main(int argc, char* argv[])
 	}
 
 	static constexpr std::array<std::string_view, 3> pwr_source = {
-		"/power1_cap_default",
-		"/power1_cap_min",
-		"/power1_cap_max"
+		"power1_cap_default"sv,
+		"power1_cap_min"sv,
+		"power1_cap_max"sv
 	};
 
 	auto pwrtarget = read_dec_uint64_value_from(hwmon / pwr_source[what_to_do]);
-	auto err = write_dec_uint64_value_to(hwmon / "/power1_cap", pwrtarget);
+	auto err = write_dec_uint64_value_to(hwmon / "power1_cap", pwrtarget);
 	if (err.value() != 0)
 		std::cerr << "Could not write: " << err.message() << std::endl;
 	else if (verbose)